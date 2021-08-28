f296b335825d49d91dd794bc0c044dfc7ecf3a51
EPeiseniece <87663601+EPeiseniece@users.noreply.github.com>
2021-08-28 22:36:01 +0300

    init

--- Pancakes/Controller/PancakeProportionsViewController.swift
+++ Pancakes/Controller/PancakeProportionsViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class PancakeProportionsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Pancakes/Controller/WebViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WebViewController.swift
+//  Pancakes
+//
+//  Created by elina.peiseniece on 28/08/2021.
+//
+
+import UIKit
+
+class WebViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
